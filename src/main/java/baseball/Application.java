2ff2df3da41726a673f2d91af5c3ab734aede4ea
sammy <sammy0329@naver.com>
2023-08-29 23:50:39 +0900

    Feat: Application Class 제작
    main 클래스로 GameManage 객체를 만들어서 gameStart 함수를 불러옴

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,14 @@
 package baseball;
 
+import baseball.utils.GameManage;
+
+import java.io.IOException;
+
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO: 프로그램 구현
+        GameManage gameManager = GameManage.getInstance();
+        gameManager.gameStart();
+
     }
 }
