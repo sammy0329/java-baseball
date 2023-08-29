77db8405feac429124e0871b6d5bd07f949b5d14
sammy <sammy0329@naver.com>
2023-08-29 23:53:48 +0900

    Test: Application 통합 테스트 제작
    - IllegalArgumentException 예외 발생 테스트
    1. 네글자 입력했을 경우 테스트
    2. 중복 숫자 입력했을 경우 테스트
    3. 각 자릿수에 1~9 이외의 숫자 입력 할 경우 테스트
    
    - 스트라이크, 볼, 낫싱 테스트

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -3,6 +3,8 @@ package baseball;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -21,15 +23,98 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    void 네글자입력_예외_테스트() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("1234"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
+    @Test
+    void 중복숫자입력_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("122"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void _1_9_이외의_숫자_입력시_예외_테스트_문자() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("12w"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void _1_9_이외의_숫자_입력시_예외_테스트_숫자0포함() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("120"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 낫싱체크() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("246", "135","2");
+                    assertThat(output()).contains("낫싱", "3스트라이크", "게임 종료");
+                },
+                1, 3, 5
+        );
+    }
+
+    @Test
+    void 쓰리볼_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("351", "513","135","2");
+                    assertThat(output()).contains("3볼", "3볼", "3스트라이크", "게임 종료");
+                },
+                1, 3, 5
+        );
+    }
+
+    @Test
+    void 투볼_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("358", "519","135","2");
+                    assertThat(output()).contains("2볼", "2볼", "3스트라이크", "게임 종료");
+                },
+                1, 3, 5
+        );
+    }
+
+    @Test
+    void 투볼_원스트라이크_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("315", "153","135","2");
+                    assertThat(output()).contains("2볼 1스트라이크", "2볼 1스트라이크", "3스트라이크", "게임 종료");
+                },
+                1, 3, 5
+        );
+    }
+
+    @Test
+    void 쓰리스트라이크_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("135","2");
+                    assertThat(output()).contains("3스트라이크", "게임 종료");
+                },
+                1, 3, 5
+        );
+    }
+
     @Override
     public void runMain() {
-        Application.main(new String[]{});
+        try {
+            Application.main(new String[]{});
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
